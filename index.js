00b0e2936e3af8cd58693c605e9f9a09b35d1552
juudcode <165465334+juudcode@users.noreply.github.com>
2024-04-07 11:50:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ async function startServer() {
       const data = {
         status: 'true',
         message: 'Bot Successfully Activated!',
-        author: 'BOTCAHX'
+        author: 'JuuDcode'
       };
       const result = {
         response: data
